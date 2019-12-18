9924691fb3551028f7a6156a5ff383d0737de822
Andrea Rocca <andrea.rocca3@gmail.com>
2019-12-18 14:14:12 +0100

    changed layout

--- pages/Games/index.vue
+++ pages/Games/index.vue
@@ -1,19 +1,15 @@
 <template lang="pug">
-  b-container.my-5
-    b-row
-      b-col(sm="12")
-        game-card(
-          v-for="(game, index) in games"
-          :key="index"
-          :game="game"
-          @click.native="$router.push(`/games/${index}`)"
-        )
-    b-row
-      b-col(sm="12")
-        b-button.shadow.w-100.bg(
-          variant="primary"
-          @click="$router.push(`/games/new`)"
-        ) Create game
+ .m-5.d-flex.flex-column
+    game-card(
+      v-for="(game, index) in games"
+      :key="index"
+      :game="game"
+      @click.native="$router.push(`/games/${index}`)"
+    )
+    b-button.shadow.w-100.bg(
+      variant="primary"
+      @click="$router.push(`/games/new`)"
+    ) Create game
 </template>
 <script>
 import GameCard from '@/components/GameCard'
